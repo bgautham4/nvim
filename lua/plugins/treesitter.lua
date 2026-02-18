29c2ba3c7233b68da45a9cd9e21ae8652ced6f9a
bgautham4 <bgautham4@gmail.com>
2026-02-18 12:47:32 +0530

    Updated packages and updated config for new treesitter api

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,56 @@
-return { -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+-- ~/.config/nvim/lua/plugins/treesitter.lua
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false, -- 🔴 REQUIRED now
+  build = ":TSUpdate",
+
+  config = function()
+    -- 1️⃣ Setup treesitter core
+    require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    -- 2️⃣ Install parsers (old ensure_installed replacement)
+    local parsers = {
+      "bash",
+      "c",
+      "cpp",
+      "diff",
+      "fish",
+      "html",
+      "latex",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "systemverilog",
+      "tcl",
+      "vim",
+      "vimdoc",
+    }
+
+    require("nvim-treesitter").install(parsers)
+
+    -- 3️⃣ Enable highlighting automatically
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup('TSStart', { clear = true }),
+      pattern = vim.treesitter.language.get_filetypes(parsers)[1],
+      callback = function()
+        --[[ Expression folding
+        vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.wo[0][0].foldmethod = 'expr'
+        ]]
+        -- indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        vim.treesitter.start()
+      end,
+    })
+
+    -- 4️⃣ Use treesitter for folding (optional but recommended)
+    -- vim.opt.foldmethod = "expr"
+    -- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  end,
 }
